2a98e6eccb4ee34071390e4cc4b42c5c76374e82
Zaquiel Rodriguez Arce <zaqroar@gmail.com>
2022-11-17 18:00:00 +0100

    feat: left icon added on header

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,21 @@
+import Image from 'next/image'
+
+const srcImage = "https://links.papareact.com/qd3";
+
 function Header() {
   return (
-    <div>
+    <header className="sticky top-0 z-50 grid grid-cols-3 white shadow-md p-5 md:px-10">
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image
+          loader={() => srcImage}
+          src={srcImage}
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
       <h1>I am the header</h1>
-    </div>
+    </header>
   );
 }
 
