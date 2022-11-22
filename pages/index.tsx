fd6faffc6578479108a5d0ff34d21bd96a26a065
Zaquiel Rodriguez Arce <zaqroar@gmail.com>
2022-11-22 18:00:00 +0100

    index updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,11 @@ const Home: NextPage = () => {
       <Banner />
       {/* Header */}
       {/* Banner */}
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+        </section>
+      </main>
       <h1>Lets build Airbnb</h1>
     </div>
   )
