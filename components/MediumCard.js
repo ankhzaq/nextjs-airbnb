4f2444e8dbd2acf3e8143e68f18ec16c76733829
Zaquiel Rodriguez Arce <gorgonizaq@gmail.com>
2022-12-16 19:00:00 +0100

    feat: mediumCard - title and styles added

--- components/MediumCard.js
+++ components/MediumCard.js
@@ -3,9 +3,10 @@ import Image from "next/image";
 function MediumCard({ img, title }) {
   return (
     <div>
-      <div>
+      <div className="relative h-80 w-80">
         <Image src={img} layout="fill" />
       </div>
+      <h3>{title}</h3>
     </div>
   );
 }
